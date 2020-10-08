2ab7c08d057b208b995c5a654fe9d86039f207c0
Chris Jantzen <jantzeca@mail.uc.edu>
2020-10-08 17:39:45 -0400

    White space

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   form = LoginForm()
-  
+
   if form.validate_on_submit():
     user = User.query.filter_by(username=form.username.data).first()
     if user and check_password_hash(user.password, form.password.data):
